a005a7aa5d265a55ce4be75fdfcecfa3cf69da9f
Sang-minKIM <kimpran@naver.com>
2024-01-07 14:53:13 +0900

    comment: fix comment

--- JS/views/pages/WagleView.js
+++ JS/views/pages/WagleView.js
@@ -7,7 +7,6 @@ export const WagleView = () => {
   /**
    * will fix
    * 서버에서 카드 정보 받기 -> cardList
-   *
    * 현재역 이전역 다음역 불러오기
    */
   const cardList = [{ card: "Card1" }, { card: "Card2" }, { card: "Card3" }]; // dummy
