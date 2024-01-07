c60ee7de8543d53db38430c80f87b7c3b1acf79e
Sang-minKIM <kimpran@naver.com>
2024-01-07 13:44:03 +0900

    feat: add tag list in upload

--- JS/data.js
+++ JS/data.js
@@ -1,13 +1,15 @@
+import tagImg from "../assets/tag_img.svg";
+
 export const tagList = [
-  { img: "", text: "출근자의 비명" },
-  { img: "", text: "지각 확정!" },
-  { img: "", text: "지옥철SOS" },
-  { img: "", text: "비상비상" },
-  { img: "", text: "칼퇴러의 환호" },
-  { img: "", text: "야근러의 무덤" },
-  { img: "", text: "야작러의 눈물" },
-  { img: "", text: "맛집 뿌시러 갑니다" },
-  { img: "", text: "날씨 요정" },
-  { img: "", text: "교수님 우리 절교해요" },
-  { img: "", text: "가벼운 발걸음 총총" },
+  { img: tagImg, text: "출근자의 비명" },
+  { img: tagImg, text: "지각 확정!" },
+  { img: tagImg, text: "지옥철SOS" },
+  { img: tagImg, text: "비상비상" },
+  { img: tagImg, text: "칼퇴러의 환호" },
+  { img: tagImg, text: "야근러의 무덤" },
+  { img: tagImg, text: "야작러의 눈물" },
+  { img: tagImg, text: "맛집 뿌시러 갑니다" },
+  { img: tagImg, text: "날씨 요정" },
+  { img: tagImg, text: "교수님 우리 절교해요" },
+  { img: tagImg, text: "가벼운 발걸음 총총" },
 ];
