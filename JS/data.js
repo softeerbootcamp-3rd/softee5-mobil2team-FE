98e7b8c2940d16887881f9f08b6dafebe49ae8bc
Sang-minKIM <kimpran@naver.com>
2024-01-07 13:37:10 +0900

    feat: add tag text

---
+++ JS/data.js
@@ -0,0 +1,13 @@
+export const tagList = [
+  { img: "", text: "출근자의 비명" },
+  { img: "", text: "지각 확정!" },
+  { img: "", text: "지옥철SOS" },
+  { img: "", text: "비상비상" },
+  { img: "", text: "칼퇴러의 환호" },
+  { img: "", text: "야근러의 무덤" },
+  { img: "", text: "야작러의 눈물" },
+  { img: "", text: "맛집 뿌시러 갑니다" },
+  { img: "", text: "날씨 요정" },
+  { img: "", text: "교수님 우리 절교해요" },
+  { img: "", text: "가벼운 발걸음 총총" },
+];
