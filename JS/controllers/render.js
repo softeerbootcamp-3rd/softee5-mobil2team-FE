3eaf2df9e6967500b3e8dd9d6154e3200f19716c
Sang-minKIM <kimpran@naver.com>
2024-01-08 16:15:58 +0900

    feat: add renderModal

--- JS/controllers/render.js
+++ JS/controllers/render.js
@@ -1,7 +1,13 @@
 import { WagleView } from "../views/pages/WagleView.js";
 
 const app = document.getElementById("app");
+const modal = document.getElementsByClassName("modal");
 
 export const renderWagle = (stationId, cardList) => {
   app.innerHTML = WagleView(stationId, cardList);
+};
+
+export const renderModal = () => {
+  const modalHTML = UploadView();
+  modal.innerHTML = modalHTML;
 };
