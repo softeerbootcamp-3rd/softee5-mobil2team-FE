5f81150ba9eb42c4b544c73f40e769157352fb47
Sang-minKIM <kimpran@naver.com>
2024-01-08 20:23:38 +0900

    feat: add img selection

--- JS/controllers/events/onClick.js
+++ JS/controllers/events/onClick.js
@@ -1,11 +1,26 @@
-import { uploadCardHandler } from "../handlers/uploadCardHandler";
+import {
+  uploadCardHandler,
+  tagSelectHandler,
+  imgSelectHandler,
+} from "../handlers/uploadCardHandler";
 
 const clickHandlerMap = {
   "js-onUpload": uploadCardHandler,
+  "js-onTagSelect": tagSelectHandler,
+  "js-onImgSelect": imgSelectHandler,
 };
 
 export const onClick = (event) => {
   const { target } = event;
+  const executeHandler = clickHandlerMap[target.classList[0]];
+  if (executeHandler) {
+    executeHandler(target);
+  }
+};
+
+export const onModalClick = (event) => {
+  const { target } = event;
+
   const executeHandler = clickHandlerMap[target.classList[0]];
   if (executeHandler) {
     executeHandler(target);
