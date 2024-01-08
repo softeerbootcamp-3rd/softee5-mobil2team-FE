be2680846037327c4343a1e4dcce9896d4861c80
Sang-minKIM <kimpran@naver.com>
2024-01-08 11:31:43 +0900

    fix: fix img src

--- JS/views/components/UploadHeaderView.js
+++ JS/views/components/UploadHeaderView.js
@@ -17,7 +17,7 @@ export const UploadHeaderView = () => {
       <circle cx="6" cy="6" r="6" fill="#00DFA9" />
     </svg>
     <h3 class="station__title__text">잠실나루역</h3>
-    <img src="./assets/triangle_img.svg" alt="triangle" />
+    <img src="../../../assets/triangle_img.svg" alt="triangle" />
   </menu>
   <h4 class="upload__header__text">에서 무슨 일이?</h4>
 </header>
