7f282920e46dbf74711c8f344a8e65fb519be31c
Sang-minKIM <kimpran@naver.com>
2024-01-09 16:14:40 +0900

    feat: add default  tag selection

--- JS/views/components/TagView.js
+++ JS/views/components/TagView.js
@@ -1,6 +1,6 @@
 export const TagView = (id, img, text) => {
   return `
-    <li class="js-onTagSelect tag" id="${id}">
+    <li class='${id ? "js-onTagSelect tag" : "js-onTagSelect tag selected-tag"}' id="${id}">
         <img class="js-onTagSelect tag__img" alt="tag-img" src="${img}" />
         <span class="js-onTagSelect tag__text" >${text}</span>
     </li>
