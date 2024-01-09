314d253600e5ad229f694cc536fd116d35517870
insiderhj <insiderhj@gmail.com>
2024-01-09 15:52:03 +0900

    fix: 태그 간단한 수정

--- JS/views/components/HotStationView.js
+++ JS/views/components/HotStationView.js
@@ -1,10 +1,11 @@
 export const HotStationView = (id, stationName) => {
   return `
-    <a data-navigate href="station/${id - 1}">
-      <li class="js-onHotStationSelect hot_station" id="hot_${id}">
+    <li>
+      <a data-navigate href="station/${id - 1}" class="js-onHotStationSelect hot_station">
         <img class="js-onHotStationSelect hot_station__img" alt="hot_station_img" src="hot_img.svg"/>
         <span class="js-onHotStationSelect ho_station__text">${stationName}</span>
-      </li>
+      </a>
+    </li>
     </a>
   `
 }
