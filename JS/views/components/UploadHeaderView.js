58463d993a34f433997b79a9d9499bd413efb253
Sang-minKIM <kimpran@naver.com>
2024-01-09 11:00:38 +0900

    fix: remove triangle

--- JS/views/components/UploadHeaderView.js
+++ JS/views/components/UploadHeaderView.js
@@ -13,7 +13,6 @@ export const UploadHeaderView = (currStation) => {
       <circle cx="6" cy="6" r="6" fill="#00DFA9" />
     </svg>
     <h3 class="station__title__text">${currStation}</h3>
-    <img src="../../../assets/triangle_img.svg" alt="triangle" />
   </menu>
   <h4 class="upload__header__text">에서 무슨 일이?</h4>
 </header>
