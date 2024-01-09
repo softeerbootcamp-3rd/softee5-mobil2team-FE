8b1cca330b1a440014cd96f1ca60c6ddfe95095c
Sang-minKIM <kimpran@naver.com>
2024-01-09 10:28:15 +0900

    feat: add textCount

--- JS/controllers/handlers/textCountHandler.js
+++ JS/controllers/handlers/textCountHandler.js
@@ -1,11 +1,10 @@
-const textCnt = document.querySelector('.upload__text-count');
-
 export const textCountHandler = (target) => {
-    const length = target.value.length;
-    textCnt.textContent = `${length}/100`;
+  const length = target.value.length;
+  const textCnt = [...target.parentNode.children][4];
+  textCnt.textContent = `${length}/100`;
 
-    if(length > 100) {
-        text.value = text.substring(0, 100);
-        textCnt.textContent = '100/100';
-    }
-}
+  if (length > 100) {
+    target.value = target.value.substring(0, 100);
+    textCnt.textContent = "100/100";
+  }
+};
