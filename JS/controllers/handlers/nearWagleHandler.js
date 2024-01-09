46888cd70fe16ac348697175b7ad01a0b0d6a9ff
Sang-minKIM <kimpran@naver.com>
2024-01-09 12:08:04 +0900

    feat: implement focus near station

--- JS/controllers/handlers/nearWagleHandler.js
+++ JS/controllers/handlers/nearWagleHandler.js
@@ -1,20 +1,23 @@
 import { getNearStation } from "../api/getApi";
 
 export const gotoNearWagle = (target) => {
-  const tooltipElement = document.querySelector('.subway-line__near-tooltip');
+  const tooltipElement = document.querySelector(".subway-line__near-tooltip");
   if (tooltipElement) {
-      tooltipElement.style.display = 'none';
+    tooltipElement.style.display = "none";
   }
-  
+
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      getNearStation(latitude, longitude);
-    }, function(error) {
-      alert('위치 정보를 가져올 수 없습니다.');
-    });
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        getNearStation(latitude, longitude, target);
+      },
+      function (error) {
+        alert("위치 정보를 가져올 수 없습니다.");
+      }
+    );
   } else {
-    alert('이 브라우저에서는 위치 정보가 지원되지 않습니다.');
+    alert("이 브라우저에서는 위치 정보가 지원되지 않습니다.");
   }
 };
