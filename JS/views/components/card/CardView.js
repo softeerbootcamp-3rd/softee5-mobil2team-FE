9c87231ae23dc7a2ea3960f7941666fc67813b4d
Sang-minKIM <kimpran@naver.com>
2024-01-13 23:35:03 +0900

    fix: remove only img card view

--- JS/views/components/card/CardView.js
+++ JS/views/components/card/CardView.js
@@ -3,7 +3,7 @@ import { tagList } from "../../../data.js";
 import { BothCardView } from "./BothCardView.js";
 import { CardFooterView } from "./CardFooterView.js";
 import { CardHeaderView } from "./CardHeaderView.js";
-import { OnlyImgCardView } from "./OnlyImgCardView.js";
+// import { OnlyImgCardView } from "./OnlyImgCardView.js";
 import { OnlyTextCardView } from "./OnlyTextCardView.js";
 
 export const CardView = ({ nickname, content, imageUrl, like, createdTime, id, tagId }) => {
@@ -11,14 +11,17 @@ export const CardView = ({ nickname, content, imageUrl, like, createdTime, id, t
   const formattedTime = getTime(createdTime);
 
   return `
-<section class="wagle__main__card" id="${id}">
-    ${CardHeaderView(profileImg, nickname, formattedTime)}
-    <article class="card__content">
-        ${!imageUrl ? OnlyTextCardView(content) : ""}
-        ${imageUrl && content ? BothCardView(content, imageUrl) : ""}
-        ${!content ? OnlyImgCardView(imageUrl) : ""}
-    </article>
-    ${CardFooterView(like)}
-</section>
-    `;
+  <section class="wagle__main__card" id="${id}">
+      ${CardHeaderView(profileImg, nickname, formattedTime)}
+      <article class="card__content">
+          ${!imageUrl ? OnlyTextCardView(content) : BothCardView(content, imageUrl)}
+      </article>
+      ${CardFooterView(like)}
+  </section>
+      `;
 };
+
+/**
+ * content가 없는 경우를 일단 없앴음
+ * ${!content ? OnlyImgCardView(imageUrl) : ""}
+ */
